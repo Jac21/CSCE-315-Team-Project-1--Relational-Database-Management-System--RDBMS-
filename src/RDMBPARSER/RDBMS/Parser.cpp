b3bef630ef873da2f85adc9f655e1236b0cf0c02
Lauren Fuller <lef592@neo.tamu.edu>
2014-02-19 18:10:52 -0600

    This is the parser.cpp and parser.h files.

--- src/RDMBPARSER/RDBMS/Parser.cpp
+++ src/RDMBPARSER/RDBMS/Parser.cpp
@@ -2,6 +2,7 @@
 #include "Relation.h"
 #include "Database.h"
 #include "Condition.h"
+#include "Parser.h"
 #include <vector>
 #include <string>
 #include <algorithm> 
@@ -378,7 +379,7 @@ public:
 
 		Comparison asdf(AttributeNameWhere, _Data(Argument), Comparison::EQUALS);
 		_Relation* Original = DB->getRelation(RelationName);
-		_Relation Result = asdf.evaluate(DB->getRelation(RelationName));	//ERROR DUE TO OUTDATED CONDITION FILE, CORRECTION CAN BE FOUND IN "MostUpDateVer2" on Dropbox
+		_Relation Result = *(asdf.evaluate(DB->getRelation(RelationName)));	//ERROR DUE TO OUTDATED CONDITION FILE, CORRECTION CAN BE FOUND IN "MostUpDateVer2" on Dropbox
 		for (int i = 0; i < Result.Columns.size(); i++)
 		{
 			for (int j = 0; j < AttributesToChange.size(); j++)
@@ -442,7 +443,7 @@ public:
 
 	void ExecuteQuery(string Input)
 	{
-
+		// NOTE: Still needs implimentation 
 	}
 
 	bool IsCommand(string Token)
@@ -458,6 +459,7 @@ public:
 	bool IsQuery(string Token)
 	{
 		return false;
+		// NOTE: Still needs implimentation 
 	}
 
 	Command* GetCommand(string Token)
@@ -633,6 +635,712 @@ vector<_Data> GrabDataSets(string S)
 
 	return AllTokens;
 }
+
+vector<string> GrabColumn(string S) {
+	string Token;
+	vector<string> AllTokens;
+
+	int Length = S.length();
+	for(int i = 0; i < Length; i++) {
+
+		if(S[i] != '(' && S[i] != ')') {
+
+			if(S[i] != ',') {
+
+				Token += S[i]; 
+
+			} else { 
+
+				AllTokens.push_back(trim(Token));
+
+			} // end if else statement 
+
+		} else {
+				
+			if(Token != "") {
+					AllTokens.push_back(trim(Token));
+					Token = "";
+			} // end if statement 
+
+		} // end if else statement 
+		
+		if(S[i] == ')') {
+
+			return AllTokens;
+
+		} // end of statement 
+	
+	}  // end for loop 
+
+} // end GrabColumn
+
+string removeParentheses(string S) { 
+
+	S = trim(S); 
+	string newString = ""; 
+	if (S[0] == '(' && S[S.size()-1] == ')') { 
+
+		for (int i=1; i < S.size()-2; i++) { 
+
+			newString += S[i]; 
+
+		} // end for loop
+
+		return newString; 
+
+	} // end if statement 
+
+	newString = S;
+	return newString;
+
+} // end removeParentheses(string)
+
+	RelationName::RelationName() { 
+
+		Identifer = ""; 
+		// NOTE: Relation name doesn't have a real identifier. 
+
+	} // end constructor 
+    
+    // sees if the input is a relation name (must start with a-z or A-Z)
+    bool RelationName::Execute(Database* DB, string input) {
+        
+		string isName = GrabToken(input);
+		isName = ltrim(input); 
+        
+		if(GrabToken(input) != "<-") {
+
+			return false;
+
+		} // end if statements 
+        
+        if (alpha(isName[0]) == false) return false;
+        
+        for (int i = 1; i < isName.size(); i++) {
+            
+            if (alpha(isName[i]) == false || digit(isName[i]) == false) {
+                
+                Identifer = ""; 
+                return false;
+                
+            } // end if statement
+            
+        } // end for loop
+        
+		// NOTE: This needs to account for new relations created by relations too in the case of relation_name <- (expr) 
+		relation = DB->getRelation(isName); 
+        return true;
+        
+    } // end execute(Database, string)
+    
+    bool RelationName::isArrow(string input) {
+        
+        string isArrow = "" + input[0] + input[1];
+        string shouldBe = "<-";
+        
+        if (isArrow.compare(shouldBe)) {
+            
+            return true;
+            
+        } // end if statement
+        
+        return false;
+        
+    } // end isArrow(string)
+    
+    bool RelationName::digit(char d) {
+        
+        switch (d) {
+            case '0' : return true;
+            case '1' : return true;
+            case '2' : return true;
+            case '3' : return true;
+            case '4' : return true;
+            case '5' : return true;
+            case '6' : return true;
+            case '7' : return true;
+            case '8' : return true;
+            case '9' : return true;
+            default : return false;
+        } // end switch statement
+        
+    } // end digit(char)
+    
+    bool RelationName::alpha(char a) {
+        
+        switch (a) {
+            case 'a' : return true;
+            case 'A' : return true;
+            case 'b' : return true;
+            case 'B' : return true;
+            case 'c' : return true;
+            case 'C' : return true;
+            case 'd' : return true;
+            case 'D' : return true;
+            case 'e' : return true;
+            case 'E' : return true;
+            case 'f' : return true;
+            case 'F' : return true;
+            case 'g' : return true;
+            case 'G' : return true;
+            case 'h' : return true;
+            case 'H' : return true;
+            case 'i' : return true;
+            case 'I' : return true;
+            case 'j' : return true;
+            case 'J' : return true;
+            case 'k' : return true;
+            case 'K' : return true;
+            case 'l' : return true;
+            case 'L' : return true;
+            case 'm' : return true;
+            case 'M' : return true;
+            case 'n' : return true;
+            case 'N' : return true;
+            case 'o' : return true;
+            case 'O' : return true;
+            case 'p' : return true;
+            case 'P' : return true;
+            case 'q' : return true;
+            case 'Q' : return true;
+            case 'r' : return true;
+            case 'R' : return true;
+            case 's' : return true;
+            case 'S' : return true;
+            case 't' : return true;
+            case 'T' : return true;
+            case 'u' : return true;
+            case 'U' : return true;
+            case 'v' : return true;
+            case 'V' : return true;
+            case 'w' : return true;
+            case 'W' : return true;
+            case 'x' : return true;
+            case 'X' : return true;
+            case 'y' : return true;
+            case 'Y' : return true;
+            case 'z' : return true;
+            case 'Z': return true;
+			case '_' : return true; 
+            default : return false;
+        } // end switch statement
+        
+    } // end alpha(char)
+
+
+	Selection::Selection() { 
+
+		Identifer = "select";
+
+	} // end constructor 
+
+	bool Selection::Execute(Database* DB,string input) { 
+
+		querys.push_back(new RelationName); 
+		querys.push_back(new Selection); 
+		querys.push_back(new Projection);
+		querys.push_back(new Renaming); 
+		querys.push_back(new SetUnion);
+		querys.push_back(new SetDifference); 
+		querys.push_back(new CrossProduct);
+		querys.push_back(new NaturalJoin); 
+
+		//NOTE: How did you do condition? This is needed here. 
+		return false; 
+
+	} // end Exectue 
+
+	Projection::Projection() { 
+
+		Identifer = "project"; 
+
+	} // end constructor 
+
+	bool Projection::Execute(Database* DB, string input) { 
+
+		querys.push_back(new RelationName); 
+		querys.push_back(new Selection); 
+		querys.push_back(new Projection);
+		querys.push_back(new Renaming); 
+		querys.push_back(new SetUnion);
+		querys.push_back(new SetDifference); 
+		querys.push_back(new CrossProduct);
+		querys.push_back(new NaturalJoin); 
+
+		input = ltrim(input);
+		vector<string> attributeList = GrabColumn(input); // NOTE: This function should grab a list of the form (attribute_name1, attribute_name2, ... , attribute_nameN)
+		std::string id; 
+		bool success; 
+		_Relation* newRelation = new _Relation(); 
+		_Relation* Relation = new _Relation();
+
+		input = removeParentheses(input); // NOTE: This should remove the parentheses around a (expr) 
+
+		for (int i=0; i < querys.size(); i++) { 
+
+			id = GrabToken(input); // NOTE: this should grab the identifier of the function (if it has one) 
+			id = trim(id); 
+
+			if (id != "select" || id != "project" || id != "rename" && i == 0) { 
+
+				id = ""; // NOTE: this should set the identifier to "" in the case of which it doesn't have a real identifier 
+
+			} // end if statement 
+
+			success = querys[i]->Execute(DB, input);  // NOTE: this should evaluate to true if it was able to parse the input for that type of query 
+			if (success && querys[i]->IsQuery(id)) { 
+
+				Relation = querys[i]->relation; // NOTE: if success was true, then we succeeded in parsing the relation 
+
+			} // end if statement
+
+		} // end for loop
+
+		if(Relation != NULL) {
+
+			newRelation = DB->projection(attributeList, *(Relation)); 
+			relation = newRelation;  
+			return true; 
+		} // end if statement 
+
+		return false;  
+
+	} // end Execute 
+
+
+	Renaming::Renaming() { 
+
+		Identifer = "rename"; 
+
+	} // end constructor 
+
+	bool Renaming::Execute(Database* DB, string input) { 
+
+		// NOTE: this is the same as Projection (pretty much) 
+		querys.push_back(new RelationName); 
+		querys.push_back(new Selection); 
+		querys.push_back(new Projection);
+		querys.push_back(new Renaming); 
+		querys.push_back(new SetUnion);
+		querys.push_back(new SetDifference); 
+		querys.push_back(new CrossProduct);
+		querys.push_back(new NaturalJoin); 
+
+		input = ltrim(input);
+		vector<string> attributeList = GrabColumn(input); 
+		string id; 
+		bool success; 
+		_Relation* newRelation = new _Relation(); 
+		_Relation* Relation = new _Relation();
+
+		input = removeParentheses(input);
+
+		for (int i=0; i < querys.size(); i++) { 
+
+			id = GrabToken(input); 
+			id = trim(id); 
+
+			if (id != "select" || id != "project" || id != "rename" && i == 0) { 
+
+				id = ""; 
+
+			} // end if statement 
+
+			success = querys[i]->Execute(DB, input); 
+			if (success && querys[i]->IsQuery(id)) { 
+
+				Relation = querys[i]->relation; 
+
+			} // end if statement
+
+		} // end for loop
+		if(Relation != NULL) {
+
+			// NOTE: This still needs the renamed relation to give to relation 
+			DB->renaming(attributeList, Relation); 
+			DB->addRelation(*(newRelation));
+			relation = newRelation;
+			return true; 
+
+		} // end if statement 
+
+		return false;  
+
+	} // end Execute 
+
+
+	SetUnion::SetUnion() { 
+
+		Identifer = ""; 
+
+	} // end constructor  
+
+	bool SetUnion::Execute(Database* DB, string input) { 
+
+		querys.push_back(new RelationName); 
+		querys.push_back(new Selection); 
+		querys.push_back(new Projection);
+		querys.push_back(new Renaming); 
+		querys.push_back(new SetUnion);
+		querys.push_back(new SetDifference); 
+		querys.push_back(new CrossProduct);
+		querys.push_back(new NaturalJoin); 
+
+		input = ltrim(input); 
+		string relation1;
+		string relation2;
+		bool r = false; 
+		for (int i = 0; i < input.size(); i++) { 
+
+			if (input[i] != '+' && r == false) { 
+
+				relation1 += input[0]; 
+
+			} else if (input[i] == '+') {  // NOTE: this won't find the correct + if there is more than one in the query (same for SetDifference, CrossProduct, and NaturalJoin) 
+
+				r = true; 
+
+			} // end if else 
+
+			if (r) { 
+
+				relation2 += input[i]; 
+
+			} // end if statement 
+
+		} // end for loop 
+
+		// This should do what Projection does to evaluate a (expr), except for two (same for SetDifference, CrossProduct, and NaturalJoin) 
+		string id1; 
+		string id2; 
+		bool success1;
+		bool success2;
+		_Relation* newRelation; 
+		_Relation* Relation1 = new _Relation();
+		_Relation* Relation2 = new _Relation(); 
+
+		relation1 = removeParentheses(relation1);
+		relation2 = removeParentheses(relation2); 
+
+		for (int i=0; i < querys.size(); i++) { 
+
+			id1 = GrabToken(relation1); 
+			id1 = trim(id1); 
+			id2 = GrabToken(relation2);
+			id2 = trim(id2);
+
+			if (id1 != "select" || id1 != "project" || id1 != "rename" && i == 0) { 
+
+				id1 = ""; 
+
+			} // end if statement 
+
+			if (id2 != "select" || id2 != "project" || id2 != "rename" && i == 0) { 
+
+				id2 = ""; 
+
+			} // end if statement 
+
+			success1 = querys[i]->Execute(DB, relation1); 
+			success2 = querys[i]->Execute(DB, relation2);
+
+			if (success1 && querys[i]->IsQuery(id1)) { 
+
+				Relation1 = querys[i]->relation; 
+
+			} // end if statement
+
+			if (success2 && querys[i]->IsQuery(id2)) { 
+
+				Relation2 = querys[i]->relation; 
+
+			} // end if statement
+
+		} // end for loop
+
+		newRelation = DB->setUnion(*(Relation1), *(Relation2)); 
+		DB->addRelation(*(newRelation)); 
+		relation = newRelation; 
+		return true; 
+
+	} // end Execute(Database*, string)
+
+
+	SetDifference::SetDifference() { 
+
+		Identifer = ""; 
+
+	} // end constructor  
+
+	bool SetDifference::Execute(Database* DB, string input) { 
+
+		querys.push_back(new RelationName); 
+		querys.push_back(new Selection); 
+		querys.push_back(new Projection);
+		querys.push_back(new Renaming); 
+		querys.push_back(new SetUnion);
+		querys.push_back(new SetDifference); 
+		querys.push_back(new CrossProduct);
+		querys.push_back(new NaturalJoin); 
+
+		input = ltrim(input); 
+		string relation1;
+		string relation2;
+		bool r = false; 
+		for (int i = 0; i < input.size(); i++) { 
+
+			if (input[i] != '-' && r == false) { 
+
+				relation1 += input[0]; 
+
+			} else if (input[i] == '-') {
+
+				r = true; 
+
+			} // end if else 
+
+			if (r) { 
+
+				relation2 += input[i]; 
+
+			} // end if statement 
+
+		} // end for loop 
+
+		string id1; 
+		string id2; 
+		bool success1;
+		bool success2;
+		_Relation* newRelation; 
+		_Relation* Relation1 = new _Relation();
+		_Relation* Relation2 = new _Relation(); 
+
+		relation1 = removeParentheses(relation1);
+		relation2 = removeParentheses(relation2); 
+
+		for (int i=0; i < querys.size(); i++) { 
+
+			id1 = GrabToken(relation1); 
+			id1 = trim(id1); 
+			id2 = GrabToken(relation2);
+			id2 = trim(id2);
+
+			if (id1 != "select" || id1 != "project" || id1 != "rename" && i == 0) { 
+
+				id1 = ""; 
+
+			} // end if statement 
+
+			if (id2 != "select" || id2 != "project" || id2 != "rename" && i == 0) { 
+
+				id2 = ""; 
+
+			} // end if statement 
+
+			success1 = querys[i]->Execute(DB, relation1); 
+			success2 = querys[i]->Execute(DB, relation2);
+
+			if (success1 && querys[i]->IsQuery(id1)) { 
+
+				Relation1 = querys[i]->relation; 
+
+			} // end if statement
+
+			if (success2 && querys[i]->IsQuery(id2)) { 
+
+				Relation2 = querys[i]->relation; 
+
+			} // end if statement
+
+		} // end for loop
+
+		newRelation = DB->setDifference(*(Relation1), *(Relation2)); 
+		DB->addRelation(*(newRelation)); 
+		relation = newRelation; 
+		return true; 
+
+	} // end Execute(Database*, string)
+
+
+	CrossProduct::CrossProduct() { 
+
+		Identifer = ""; 
+
+	} // end constructor  
+
+	bool CrossProduct::Execute(Database* DB, string input) { 
+
+		querys.push_back(new RelationName); 
+		querys.push_back(new Selection); 
+		querys.push_back(new Projection);
+		querys.push_back(new Renaming); 
+		querys.push_back(new SetUnion);
+		querys.push_back(new SetDifference); 
+		querys.push_back(new CrossProduct);
+		querys.push_back(new NaturalJoin); 
+
+		input = ltrim(input); 
+		string relation1;
+		string relation2;
+		bool r = false; 
+		for (int i = 0; i < input.size(); i++) { 
+
+			if (input[i] != '*' && r == false) { 
+
+				relation1 += input[0]; 
+
+			} else if (input[i] == '*') {
+
+				r = true; 
+
+			} // end if else 
+
+			if (r) { 
+
+				relation2 += input[i]; 
+
+			} // end if statement 
+
+		} // end for loop 
+
+		string id1; 
+		string id2; 
+		bool success1;
+		bool success2;
+		_Relation* newRelation; 
+		_Relation* Relation1 = new _Relation();
+		_Relation* Relation2 = new _Relation(); 
+
+		relation1 = removeParentheses(relation1);
+		relation2 = removeParentheses(relation2); 
+
+		for (int i=0; i < querys.size(); i++) { 
+
+			id1 = GrabToken(relation1); 
+			id1 = trim(id1); 
+			id2 = GrabToken(relation2);
+			id2 = trim(id2);
+
+			if (id1 != "select" || id1 != "project" || id1 != "rename" && i == 0) { 
+
+				id1 = ""; 
+
+			} // end if statement 
+
+			if (id2 != "select" || id2 != "project" || id2 != "rename" && i == 0) { 
+
+				id2 = ""; 
+
+			} // end if statement 
+
+			success1 = querys[i]->Execute(DB, relation1); 
+			success2 = querys[i]->Execute(DB, relation2);
+
+			if (success1 && querys[i]->IsQuery(id1)) { 
+
+				Relation1 = querys[i]->relation; 
+
+			} // end if statement
+
+			if (success2 && querys[i]->IsQuery(id2)) { 
+
+				Relation2 = querys[i]->relation; 
+
+			} // end if statement
+
+		} // end for loop
+
+		newRelation = DB->crossProduct(*(Relation1), *(Relation2)); 
+		DB->addRelation(*(newRelation)); 
+		relation = newRelation;
+		return true; 
+
+	} // end Execute(Database*, string)
+
+
+	NaturalJoin::NaturalJoin() { 
+
+		Identifer = ""; 
+
+	} // end constructor 
+
+bool NaturalJoin::Execute(Database* DB, string input) { 
+
+		querys.push_back(new RelationName); 
+		querys.push_back(new Selection); 
+		querys.push_back(new Projection);
+		querys.push_back(new Renaming); 
+		querys.push_back(new SetUnion);
+		querys.push_back(new SetDifference); 
+		querys.push_back(new CrossProduct);
+		querys.push_back(new NaturalJoin); 
+
+		input = ltrim(input); 
+		string relation1 = GrabToken(input);
+		string join = GrabToken(input); // NOTE: it should be able to grab JOIN here
+		string relation2 = GrabToken (input);
+		bool r = false; 
+
+		if (join != "JOIN") { 
+
+			return false; 
+
+		} // end if statement 
+		
+		string id1; 
+		string id2; 
+		bool success1;
+		bool success2;
+		_Relation* newRelation; 
+		_Relation* Relation1 = new _Relation();
+		_Relation* Relation2 = new _Relation(); 
+
+		relation1 = removeParentheses(relation1);
+		relation2 = removeParentheses(relation2);
+
+		for (int i=0; i < querys.size(); i++) { 
+
+			id1 = GrabToken(relation1); 
+			id1 = trim(id1); 
+			id2 = GrabToken(relation2);
+			id2 = trim(id2);
+
+			if (id1 != "select" || id1 != "project" || id1 != "rename" && i == 0) { 
+
+				id1 = ""; 
+
+			} // end if statement 
+
+			if (id2 != "select" || id2 != "project" || id2 != "rename" && i == 0) { 
+
+				id2 = ""; 
+
+			} // end if statement 
+
+
+			success1 = querys[i]->Execute(DB, relation1); 
+			success2 = querys[i]->Execute(DB, relation2);
+
+			if (success1 && querys[i]->IsQuery(id1)) { 
+
+				Relation1 = querys[i]->relation; 
+
+			} // end if statement
+
+			if (success2 && querys[i]->IsQuery(id2)) { 
+
+				Relation2 = querys[i]->relation; 
+
+			} // end if statement
+
+		} // end for loop
+
+		newRelation = DB->crossProduct(*(Relation1), *(Relation2)); 
+		DB->addRelation(*(newRelation)); 
+		relation = newRelation;
+
+	} // end Execute(Database*, string)
+
 
 int main()
 {
